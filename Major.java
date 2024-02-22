971f500da2bbf5fbc2934c861782c7f4daf31130
Abhinav Krishniah <abhinavk@Abhinavs-MacBook-Air-2.local>
2024-02-22 14:30:24 -0500

    finshed #31

---
+++ Major.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Major {
+    private UUID id;
+    private String majorName;
+    private ArrayList<Course> requiredCourses;
+    private EightSemesterPlan majorMap;
+
+    public Major(UUID id, String majorName, ArrayList<Course> requiredCourses, EightSemesterPlan majorMap) {
+        this.id = id;
+        this.majorName = majorName;
+        this.requiredCourses = requiredCourses;
+        this.majorMap = majorMap;
+    }
+}
