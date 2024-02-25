8bda4f61af91a3189d2eae15737ffd1d13395e3e
solimamab <120787343+solimamab@users.noreply.github.com>
2024-02-25 14:32:52 -0500

    finished #32

---
+++ EightSemesterPlan.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+class EightSemesterPlan {
+    private ArrayList<Course> classesInPlan;
+    private ApplicationArea applicationArea;
+    private ArrayList<ElectiveChoices> electiveChoice;
+    private double majorProgress;
+
+    public EightSemesterPlan(ArrayList<Course> plan) {
+        this.classesInPlan = plan;
+        this.applicationArea = new ApplicationArea(); 
+        this.electiveChoice = new ArrayList<>();
+        this.majorProgress = 0.0;
+    }
+
+    public void addClassToPlan(Course course, EightSemesterPlan plan) {
+        plan.classesInPlan.add(course);
+    }
+
+    public void removeClassToPlan(Course course, EightSemesterPlan plan) {
+        plan.classesInPlan.remove(course);
+    }
+
+    public void viewPlan(EightSemesterPlan plan) {
+        for (Course course : plan.classesInPlan) {
+            System.out.println(course);
+        }
+    }
+
+    public double getMajorProgress() {
+        return this.majorProgress;
+    }
+
+    public void viewElectiveChoices(ArrayList<ElectiveChoices> choices) {
+        for (ElectiveChoices choice : choices) {
+            System.out.println(choice); 
+        }
+    }
+
+}
