85090260af280531046c1654dae6794f46c0a2bc
solimamab <120787343+solimamab@users.noreply.github.com>
2024-02-27 21:04:40 -0500

    fixing #14

--- Student.java
+++ Student.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
+import java.util.UUID;
+import java.util.ArrayList;
 
 
-public class UndergradStudent extends User{
+public class Student extends User{
     private Classification classification;
+    private UUID id;
     private int completedCreditHours;
     private int remainingCreditHours;
-    private Flag flag;
+    private String flag;
     private double overallGPA;
     private Major major;
     private String minor;
@@ -15,20 +18,20 @@ public class UndergradStudent extends User{
     private EightSemesterPlan eightSemesterPlan;
     private ArrayList<Course> currentCourses;
 
-    public UndergradStudent(String username, String password, String firstname, String lastname, Classification classification, int completedCreditHours, int remainingCreditHours, Flag flag, double overallGPA, Major major, String minor, Boolean FEPRA, Advisor advisor, EightSemesterPlan eightSemesterPlan, ArrayList<Course> currentCourses) {
+    public Student(String username, String password, String firstname, String lastname, Classification classification, int completedCreditHours, int remainingCreditHours, String flag, double overallGPA, Major major, String minor, Boolean FEPRA, Advisor advisor, EightSemesterPlan eightSemesterPlan, ArrayList<Course> currentCourses) {
         super(username, password, firstname, lastname); 
-        this.classification = new Classification();
-        this.completedCreditHours = 0;
-        this.remainingCreditHours = 0;
-        this.flag = new Flag();
-        this.overallGPA = 0.0;
-        this.major = new Major();
-        this.minor = "";
-        this.FEPRA = false;
-        this.advisor = new Advisor();
-        this.completedCourses = new ArrayList<>();
-        this.eightSemesterPlan = new EightSemesterPlan();
-        this.currentCourses = new ArrayList<>();
+                        this.classification = classification;
+                        this.completedCreditHours = 0;
+                        this.remainingCreditHours = 0;
+                        this.flag = "None"; // Instantiate the Flag class using the appropriate constructor
+                        this.overallGPA = 0.0;
+                        this.id = UUID.randomUUID();
+                        this.major = new Major(id, minor, currentCourses, eightSemesterPlan);
+                        this.minor = "";
+                        this.FEPRA = false;
+                        this.completedCourses = new ArrayList<>();
+                        this.eightSemesterPlan = new EightSemesterPlan(currentCourses);
+                        this.currentCourses = new ArrayList<>();
     }
 
     public void setCompletedHours(ArrayList<CompletedCourse> courses) {
@@ -41,7 +44,7 @@ public class UndergradStudent extends User{
     }
 
     public void setFlag(Flag flag) {
-        this.flag = flag;
+        this.flag = "flag";
     }
 
     public void viewAvailableCourses(ArrayList<Course> availableCourses) {
@@ -61,7 +64,7 @@ public class UndergradStudent extends User{
     }
 
     public void addCompletedCourse(Course course) {
-        this.completedCourses.add(course);
+        System.out.println("Course added: " + course);
     }
 
     public void viewEightSemesterPlan(EightSemesterPlan eightSemesterPlan) {
