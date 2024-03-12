7ece7913def6f281eec669f9ae8eef9db158287b
Abhinav Krishniah <abhinavk@email.sc.edu>
2024-03-11 22:55:26 -0400

    some work on implementing prerequsites

--- CompletedCourse.java
+++ CompletedCourse.java
@@ -1,63 +1,16 @@
-/**
- * This class represents a course that has been completed and stores the grade received in that class
- */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
+
 public class CompletedCourse extends Course {
-    private Grade letterGrade;
+    private String letterGrade;
     private double qualityPoints;
-
-    /**
-     * Constructor for the completed course
-     * @param course
-     * @param letterGrade
-     * @param qualityPoints
-     */
-    public CompletedCourse(Course course, Grade letterGrade) {
-        super(course.getId(),course.getName(),course.getDepartment(),course.getNumber(),course.getDescription(),
-        course.getCreditHours(),course.getAvailablity(),course.getPrerequisite(),null);
+    
+    public CompletedCourse(UUID id, String name, String department, long number, String description, long creditHours,
+    Availablity availability, HashMap<UUID, String> prerequisite, ArrayList<UUID> corequisite,
+    String letterGrade, double qualityPoints) {
+        super(id, name, department, number, description, creditHours, availability, prerequisite, corequisite);
         this.letterGrade = letterGrade;
-    }
-
-    /**
-     * Getter for letter Grade
-     * @return the letter grade
-     */
-    public Grade getLetterGrade()
-    {
-        return this.letterGrade;
-    }
-
-    /**
-     * getter for quality points
-     * @return the number of quality points
-     */
-    public double getqualityPoints()
-    {
-        return this.qualityPoints;
-    }
-
-    /**
-     * Setter for quality points based on the USC GPA scale
-     */
-    public void setQualityPoints()
-    {
-        double points = 0;
-        if (letterGrade.equals(Grade.A))
-            points = 4;
-        else if (letterGrade.equals(Grade.B_plus))
-            points = 3.5;
-        else if (letterGrade.equals(Grade.B_plus))
-            points = 3;
-        else if (letterGrade.equals(Grade.C_plus))
-            points = 2.5;
-        else if (letterGrade.equals(Grade.C))
-            points = 2;
-        else if (letterGrade.equals(Grade.D_plus))
-            points = 1.5;
-        else if (letterGrade.equals(Grade.D))
-            points = 1;
-        else
-            points = 0;
-        
-        this.qualityPoints = points * super.getCreditHours();
+        this.qualityPoints = qualityPoints;
     }
 }
