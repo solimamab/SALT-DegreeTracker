61993dab048d564756eb2293a3a3cd38fb407834
tlilly5 <tlilly@email.sc.edu>
2024-03-11 20:08:47 -0400

    added getters to completed course class

--- CompletedCourse.java
+++ CompletedCourse.java
@@ -1,11 +1,38 @@
+/**
+ * This class represents a course that has been completed and stores the grade received in that class
+ */
 public class CompletedCourse extends Course {
     private String letterGrade;
-    private double quialityPoints;
+    private double qualityPoints;
 
-    public CompletedCourse(Course course, String letterGrade, double quialityPoints) {
+    /**
+     * Constructor for the completed course
+     * @param course
+     * @param letterGrade
+     * @param qualityPoints
+     */
+    public CompletedCourse(Course course, String letterGrade, double qualityPoints) {
         super(course.getId(),course.getName(),course.getDepartment(),course.getNumber(),course.getDescription(),
         course.getCreditHours(),course.getAvailablity(),course.getPrerequisite(),null);
         this.letterGrade = letterGrade;
-        this.quialityPoints = quialityPoints;
+        this.qualityPoints = qualityPoints;
+    }
+
+    /**
+     * Getter for letter Grade
+     * @return the letter grade
+     */
+    public String getLetterGrade()
+    {
+        return this.letterGrade;
+    }
+
+    /**
+     * getter for quality points
+     * @return the number of quality points
+     */
+    public double getqualityPoints()
+    {
+        return this.qualityPoints;
     }
 }
