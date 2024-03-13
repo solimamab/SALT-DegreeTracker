81563fe1d4c710c52b8aaee6ea3e093b8d9e2c22
Abhinav Krishniah <abhinavk@email.sc.edu>
2024-03-12 21:02:35 -0400

    updated user class

--- User.java
+++ User.java
@@ -1,91 +1,99 @@
 import java.util.UUID;
+
 /**
- * 
+ * User class
  */
 public class User {
     private String username;
     private String password;
     private String firstName;
     private String lastName;
-    protected UUID id;
-    public boolean loggedIn;
-    
+    private UUID id;
+    private boolean loggedIn;
+
     /**
-     * 
+     * Constructor that initializes a user
      * @param username
      * @param password
      * @param firstName
-     * @param lastname
+     * @param lastName
      */
-    public User(String username, String password, String firstName, String lastname)
-    {
+    public User(String username, String password, String firstName, String lastName) {
         this.username = username;
         this.password = password;
         this.firstName = firstName;
-        this.lastName = lastname;
-        this.id = new UUID(0, 0); //not sure what to pass in
+        this.lastName = lastName;
+        this.id = UUID.randomUUID(); // Generate a random UUID
     }
 
     /**
-     * 
+     * Gets user's username
      * @return
      */
-    public String getUsername()
-    {
-        return this.username;
-    }
-
-    public String getId()
-    {
-        return this.id.toString();
+    public String getUsername() {
+        return username;
     }
 
     /**
-     * 
+     * Gets user's id
      * @return
      */
-    public String getPassword()
-    {
-        return this.password;
+    public String getUserId() {
+        return id.toString();
     }
 
     /**
-     * 
+     * Gets user's password
      * @return
      */
-    public String getFirstName()
-    {
-        return this.firstName;
+    public String getPassword() {
+        return password;
     }
 
     /**
-     * 
+     * Gets user's firstname
      * @return
      */
-    public String getLastName()
-    {
-        return this.lastName;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public User getUser()
-    {
-        return new User("asmith", "12345", "amy", "smith");
+    /**
+     * Get's users lastname
+     * @return
+     */
+    public String getLastName() {
+        return lastName;
     }
 
-    public String toString(){
-        return "username:" + username + "\n" + "password:" + password + "\n" + "firstname:" + firstName + "\n" + "lastname: " + lastName;
+    /**
+     * toString representation of a user
+     */
+    public String toString() {
+        return "username:" + username + "\n" + "password:" + password + "\n" + "firstname:" + firstName + "\n"
+                + "lastname: " + lastName;
     }
 
+    /**
+     * Login method for the facade that logs a user in if their username and password match
+     * @param username
+     * @param password
+     * @return
+     */
     public boolean facadeLogin(String username, String password) {
-        return login(username, password);
-    }
-
-    private boolean login(String username, String passsword) {
         if (this.username.equals(username) && this.password.equals(password)) {
             this.loggedIn = true;
             return true;
         }
         this.loggedIn = false;
         return false;
+    }
+
+    /**
+     * Checks to see if a user has logged in
+     * @return
+     */
+    public boolean isLoggedIn() {
+        return loggedIn;
     }
 }
