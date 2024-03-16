199474e8c992a96278999a0a8309510b3b3ca5b5
tlilly5 <tlilly@email.sc.edu>
2024-03-16 15:39:26 -0400

    removed major enum

--- Majors.java
+++
@@ -1,9 +0,0 @@
-/**
- * This is an enumeration of the possible majors to take at USC
- * This adds room for expansion of more majors later down the road
- * @author Team SALT
- */
-public enum Majors {
-    CSCE,
-    CIS
-}
