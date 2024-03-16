5b945972f11953a569a7af54a3f9821b63dd6de5
tlilly5 <tlilly@email.sc.edu>
2024-03-15 22:00:15 -0400

    Worked on Appplication area

--- ApplicationArea.java
+++ ApplicationArea.java
@@ -1,11 +1,15 @@
 import java.util.ArrayList;
 
 public class ApplicationArea {
+    private CourseList courseList = CourseList.getInstance();
     private ArrayList<Course> applicationArea;
     private String typeofApplicationArea;
+    private ArrayList<String> typeOfAppAreas;
 
     public ApplicationArea(String type){
         this.typeofApplicationArea = type;
+        this.typeOfAppAreas = new ArrayList<String>();
+        setPossibleAppAreas();
         setCoursesNeeded();
     }
 
@@ -14,22 +18,37 @@ public class ApplicationArea {
         if (typeofApplicationArea.equals("Science"))
         {
             this.applicationArea = new ArrayList<Course>();
+            this.applicationArea.add(courseList.getCourseByNumber("CHEM", "105"));
+            this.applicationArea.add(courseList.getCourseByNumber(" ", " "));
+            this.applicationArea.add(courseList.getCourseByNumber("", ""));
         }
         else if (typeofApplicationArea.equals("Math"))
         {
             this.applicationArea = new ArrayList<Course>();
+            this.applicationArea.add(courseList.getCourseByNumber("CHEM", "105"));
+            this.applicationArea.add(courseList.getCourseByNumber(" ", " "));
+            this.applicationArea.add(courseList.getCourseByNumber("", ""));
         }
         else if (typeofApplicationArea.equals("Digital Design"))
         {
             this.applicationArea = new ArrayList<Course>();
+            this.applicationArea.add(courseList.getCourseByNumber("CHEM", "105"));
+            this.applicationArea.add(courseList.getCourseByNumber(" ", " "));
+            this.applicationArea.add(courseList.getCourseByNumber("", ""));
         }
         else if (typeofApplicationArea.equals("Speech"))
         {
             this.applicationArea = new ArrayList<Course>();
+            this.applicationArea.add(courseList.getCourseByNumber("CHEM", "105"));
+            this.applicationArea.add(courseList.getCourseByNumber(" ", " "));
+            this.applicationArea.add(courseList.getCourseByNumber("", ""));
         }
         else if (typeofApplicationArea.equals("Robitics"))
         {
             this.applicationArea = new ArrayList<Course>();
+            this.applicationArea.add(courseList.getCourseByNumber("CHEM", "105"));
+            this.applicationArea.add(courseList.getCourseByNumber(" ", " "));
+            this.applicationArea.add(courseList.getCourseByNumber("", ""));
         }
         else 
         {
@@ -37,6 +56,19 @@ public class ApplicationArea {
         }
     }
 
+    private void setPossibleAppAreas()
+    {
+        this.typeOfAppAreas.add("Science");
+        this.typeOfAppAreas.add("Math");
+        this.typeOfAppAreas.add("Digital Design");
+        this.typeOfAppAreas.add("Speech");
+        this.typeOfAppAreas.add("Robitics");
+
+    }
+    public String getListofAppAreas()
+    {
+        return "Possible Applications Areas are:\n" + typeOfAppAreas.toString();
+    }
     public ArrayList<Course> getApplicationArea()
     {
         return this.applicationArea;
