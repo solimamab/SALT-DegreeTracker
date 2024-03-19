88ca3ccd63bf95f6791b15368c980494afba4c43
Abhinav Krishniah <abhinavk@email.sc.edu>
2024-03-19 14:16:25 -0400

    setting up AdvisorTest

--- test-classes/AdvisorTest.java
+++ test-classes/AdvisorTest.java
@@ -1,1 +1,18 @@
-// just adding something here so it shows up on github
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class AdvisorTest {
+
+    @Test
+    public void testConstructorId() {
+        
+    }
+}
