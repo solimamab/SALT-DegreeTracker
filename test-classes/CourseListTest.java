4a7b0a2f56aebbfda7c8896a4deb3ce687f622d6
leemoorman <Leethalweapon487@gmail.com>
2024-03-21 13:51:46 -0400

    added Tests and tester_course.json

---
+++ test-classes/CourseListTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+/*
+ * Tested by Lee Moorman 
+ */
+
+public class CourseListTest {
+    
+}
