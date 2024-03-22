2d22c22a96b34b3e192b8cf87c764974e0b3ee73
tlilly5 <tlilly@email.sc.edu>
2024-03-22 17:10:58 -0400

    tested the user class

--- test-classes/UserTest.java
+++ test-classes/UserTest.java
@@ -10,18 +10,35 @@ import org.junit.jupiter.api.Test;
  * User Class has been tested by Tristen 
  */
 public class UserTest {
+    private User currentUser =  new User("Linda", "123456", "Linda", "Blaze");
+   
 
+    // Testing the facadeLogin Method 
     @Test
-    public void testBasicConstructor()
+    public void testValidLogin()
     {
-
+        boolean test = currentUser.facadeLogin("Linda", "123456");
+        assertEquals(true, test);
     }
 
     @Test
-    public void testForDuplicateUsername()
+    public void Notvalidlogin()
     {
-
+        boolean test = currentUser.facadeLogin("bob", "ross");
+        assertEquals(false, test);
     }
 
-    
+    @Test
+    public void onlyusername()
+    {
+        boolean test = currentUser.facadeLogin("Linda", " ");
+        assertEquals(false, test);
+    }
+
+    @Test
+    public void noUsername()
+    {
+        boolean test = currentUser.facadeLogin(" ", "123456 ");
+        assertEquals(false, test);
+    }
 }
