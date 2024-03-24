4d1ed20acd3b5ecb55c06c51d3b9e094a5473409
leemoorman <Leethalweapon487@gmail.com>
2024-03-24 18:08:00 -0400

    added test methods

--- test-classes/CourseTest.java
+++ test-classes/CourseTest.java
@@ -10,32 +10,53 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
 
+
 /*
  * Tested by Lee Moorman
  */
 
 public class CourseTest {
-    private UUID id;
-    private String name;
-    private String department;
-    private String number;
-    private String description;
-    private long creditHours;
-    private ArrayList<Availablity> availablity;
-    private HashMap<UUID, String> prerequisite;
-    private ArrayList<UUID> corequisite; 
+    private Course course;
+    private UUID uuid = UUID.randomUUID();
+    private ArrayList<Availablity> availability;
+    private HashMap<UUID, String> prerequisites;
+    private ArrayList<UUID> corequisitesIDs;
 
-    @Test
-    public void testConstructor(){
-
+    @BeforeEach
+    public void setup(){
+        availability = new ArrayList<Availablity>();
+        prerequisites = new HashMap<UUID, String>();
+        corequisitesIDs = new ArrayList<UUID>();
+        course = new Course(uuid, "TestName", "TestDept", "111", "TestDescription", 3, availability, prerequisites, corequisitesIDs);
     }
 
     @Test
-    public void testViewCouseDetails(){
-
+    public void testConstructorId(){
+        assertEquals(uuid, course.getId());
     }
 
-    public void testToString(){
-
+    @Test
+    public void testConstructorAvailability(){
+        assertEquals(availability, course.getAvailablity());
     }
+
+    @Test 
+    public void testConstructorPrerequisites(){
+        assertEquals(prerequisites, course.getPrerequisite());
+    }
+
+    @Test
+    public void testConstructorCorequisitesId(){
+        assertEquals(corequisitesIDs, course.getCorequisite());
+    }
+
+    @Test
+    public void testConstructorDetails(){
+        assertEquals("TestName", course.getName());
+        assertEquals("TestDept", course.getDepartment());
+        assertEquals("111", course.getNumber());
+        assertEquals("TestDescription", course.getDescription());
+        assertEquals(3, course.getCreditHours());
+    }
+
 }
